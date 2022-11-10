6095c7384f52071c15d5f6666d9611cab09b530c
Ronan <roleary81@gmail.com>
2022-11-10 01:01:28 +0100

    shoot

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function hello(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function hello(req, res) {
+  
+  const randmchars = await fetch('https://rickandmortyapi.com/api/character/1,2');
+  const results = await randmchars.json(); 
+
+
+  res.status(200).json({ name: 'Ro', results: results })
+
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ allo }) {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    // setLoading(true)
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        console.log(data);
+      })
+  }, [])
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -11,45 +23,26 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
+          Welcome to <a href="https://nextjs.org">Next.js</a> on Docker{data['name'] ? `, ${data['name']}!` : `!`}
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Here goes: 
+          <ul>
+            {data['results'] ? data['results'].map((r, i) => {
+                return (
+                  <li key={i}>
+                    <p>{r.name}</p>
+                    <img src={r.image} />
+                  </li>
+                );
+            }): <li>Nada yet</li>}
+
+          </ul>
+         
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      
       </main>
 
       <footer className={styles.footer}>
@@ -65,3 +58,4 @@ export default function Home() {
     </div>
   )
 }
+
