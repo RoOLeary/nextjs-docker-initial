8e5f207e9817e4b00c334d5866ea4ae6da833747
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-10 17:12:31 +0100

    some scratch config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: 'standalone',
+  i18n: {
+    // The locales you want to support in your app
+    locales: ["nl", "de", "fr"],
+    // The default locale you want to be used when visiting a non-locale prefixed path e.g. `/hello`
+    defaultLocale: "nl",
+  },
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// eslint-disable-next-line react/prop-types
+function MyApp({Component, pageProps}) {
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
