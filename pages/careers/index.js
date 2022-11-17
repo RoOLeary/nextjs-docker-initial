f89cc9f3acadc659f2804c52be60e9c7ae2b7d5f
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-17 09:45:39 +0100

    update api directory with preview mode from Craft

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -1,4 +1,4 @@
-import MainLayout from '../../Components/Globals/Layouts/MainLayout';
+import MainLayout from '../../components/Globals/Layouts/MainLayout';
 import { useRouter } from 'next/router'; 
 
 export default function Careers() {
