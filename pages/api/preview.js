dc6557d2642204ae498f26851e2a82b71ffec96b
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-17 16:20:11 +0100

    changes to /api/preview.js

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,18 +1,23 @@
 export default async (req, res) => {
-    console.log(req)
-    if (req.query.token === null) {
-      return res.status(401).json({ message: 'No preview token' })
+    // Check the secret and next parameters
+    // This secret should only be known to this API route and the CMS
+    if (req.query.secret !== 'MY_SECRET_TOKEN' || !req.query.slug) {
+      return res.status(401).json({ message: 'Invalid token' })
     }
-    if (req.query.uri === null) {
-      return res.status(401).json({ message: 'No URI provided' })
+  
+    // Fetch the headless CMS to check if the provided `slug` exists
+    // getPostBySlug would implement the required fetching logic to the headless CMS
+    const post = await getPostBySlug(req.query.slug)
+  
+    // If the slug doesn't exist prevent preview mode from being enabled
+    if (!post) {
+      return res.status(401).json({ message: 'Invalid slug' })
     }
-    res.setPreviewData(
-      {
-        token: req.query.token ?? null,
-      },
-      {
-        maxAge: 60,
-      }
-    )
+  
+    // Enable Preview Mode by setting the cookies
+    res.setPreviewData({})
+  
+    // Redirect to the path from the fetched post
+    // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
     res.redirect(`/${req.query.uri}`)
   }
