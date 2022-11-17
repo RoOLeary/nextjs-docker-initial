ec7f3f881f82d0703bb6fa8ae5b895f76fb5cc33
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-17 17:02:49 +0100

    change api preview. Managed to break all of the things

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,23 +1,42 @@
 export default async (req, res) => {
-    // Check the secret and next parameters
-    // This secret should only be known to this API route and the CMS
-    if (req.query.secret !== 'gbNV~A7ttO1HRGh' || !req.query.slug) {
-      return res.status(401).json({ message: 'Invalid token' })
+    console.log(req)
+    if (req.query.token === null) {
+      return res.status(401).json({ message: 'No preview token' })
     }
-  
-    // Fetch the headless CMS to check if the provided `slug` exists
-    // getPostBySlug would implement the required fetching logic to the headless CMS
-    const post = await getPostBySlug(req.query.slug)
-  
-    // If the slug doesn't exist prevent preview mode from being enabled
-    if (!post) {
-      return res.status(401).json({ message: 'Invalid slug' })
+    if (req.query.uri === null) {
+      return res.status(401).json({ message: 'No URI provided' })
     }
-  
-    // Enable Preview Mode by setting the cookies
-    res.setPreviewData({})
-  
-    // Redirect to the path from the fetched post
-    // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
+    res.setPreviewData(
+      {
+        token: req.query.token ?? null,
+      },
+      {
+        maxAge: 60,
+      }
+    )
     res.redirect(`/${req.query.uri}`)
   }
+
+//   export default async (req, res) => {
+//     // Check the secret and next parameters
+//     // This secret should only be known to this API route and the CMS
+//     if (req.query.secret !== 'MY_SECRET_TOKEN' || !req.query.slug) {
+//       return res.status(401).json({ message: 'Invalid token' })
+//     }
+  
+//     // Fetch the headless CMS to check if the provided `slug` exists
+//     // getPostBySlug would implement the required fetching logic to the headless CMS
+//     const post = await getPostBySlug(req.query.slug)
+  
+//     // If the slug doesn't exist prevent preview mode from being enabled
+//     if (!post) {
+//       return res.status(401).json({ message: 'Invalid slug' })
+//     }
+  
+//     // Enable Preview Mode by setting the cookies
+//     res.setPreviewData({})
+  
+//     // Redirect to the path from the fetched post
+//     // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
+//     res.redirect(`/${req.query.uri}`)
+//   }
