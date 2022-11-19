35b31e00ab0e2539e170da9f544ee81e7a8c3aa0
Ronan <roleary81@gmail.com>
2022-11-19 15:50:45 +0100

    enough for the moment

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,17 @@
 import MainLayout from '../components/Globals/Layouts/MainLayout'
 import { GetServerSideProps } from 'next';
 
-const About = ({ page, preview }) => {
+const About = ({ page, preview }:any) => {
 
     const { title } = page;
-
+    console.log(page);
     return(
         <MainLayout>
             {preview ? <h1>In Preview Mode</h1> : null}
             <section className="b-text c-section" id="about">
                 <div className="o-wrapper">
                     <div>
-                      <h1>{page.title}</h1>
+                      <h1>{title}</h1>
                       Some random stuff
                     </div>
                 </div>
@@ -25,9 +25,9 @@ const About = ({ page, preview }) => {
 
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    console.log(context);
+    // console.log(context);
     const slug = context?.query?.slug ? context.query.slug : 'about'
-    const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/articles/${slug}.json`);
+    const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/articles/about.json`);
     const data = await res.json();
 
     const preview = context.preview;
@@ -36,7 +36,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     if(preview){
         // console.log('preview is true');
         const previewData = context.previewData;
-        const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/${slug}.json?token=${previewData['token']}`);
+        const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/about.json?token=${previewData['token']}`);
         prevData = await prevResponse.json()
         // console.log(prevData);
     } 
