f11e4b8a783cceae015b20df486fd5e3ddd348ff
Ronan <roleary81@gmail.com>
2022-11-19 22:46:40 +0100

    can't believe I wasted a day researching this

--- pages/all-components.tsx
+++ pages/all-components.tsx
@@ -1,85 +1,64 @@
-import { useState, useEffect } from 'react'
-import Head from 'next/head'
-import MainLayout from '../components/Globals/Layouts/MainLayout'
-import Faq from '../components/Faq/Faq'
-import Hero from '../components/Hero'
-import BoxedImage from '../components/BoxedImage'
-import Button from '../components/Button'
-import Cards from '../components/Cards/Cards'
-// import Contact from '../components/Contact/Contact'
-import CTA from '../components/CTA/CTA'
-import Chapters from '../components/Chapters/Chapters'
-import MapComponent from '../components/Map/MapComponent'
-import MiniBanner from '../components/MiniBanner'
-import Note from '../components/Note'
-import PageBlocks from '../components/PageBlocks'
-import USPs from '../components/USPs'
-import { GetStaticProps } from 'next';
-import { useTypingText } from '../hooks/useTypingText';
+import { useRouter } from 'next/router'
+import MainLayout from './../components/Globals/Layouts/MainLayout'
+import PageBlocks from './../components/PageBlocks'
+import { GetServerSideProps } from 'next'
 
-const AllComponents = ({ page, preview }) => {
-	
-	let content = page.pageBlocks;
+const AllComponents = ({ page, preview }:any) => {
 
-	return (
-		<>
-		<Head>
-			<title>PicNext Prototype</title>
-			<link rel="icon" href="/favicon.ico" />
-		</Head>
-		<MainLayout>	
-			<main className={'main'}>
-				{preview ? `previewing ${page.title}` : ''}
-				<h1>{page.title}</h1>
-				<PageBlocks content={content} />
-			</main>
-		</MainLayout>
-		</>
-	)
-}
+    const router = useRouter()
+    const slug = router.query
+    const {title, body} = page;
 
+    let content = page.blocks;
+    console.log(content);
+    return (
+        <MainLayout>
+           {preview ? <h1>In Preview Mode</h1> : null}
+            <section className="b-text  c-section" id="learn-more">
+                <div className="o-wrapper">
+                    
+                    <div>
+                        <h1>{title}</h1>
+                        <div className={"c-formatted"} dangerouslySetInnerHTML={{__html: body }} />  
+                    </div>
+                </div>
+               
+            </section>
+            
+            <PageBlocks content={content} />
 
-export const getStaticProps: GetStaticProps = async (context) => {
-    // console.log(context);
+            {/* <Related related={entry.manualRelatedArticles} currentslug={entry.slug} /> */}
+        
+        </MainLayout>
+    )
+  }
+  
+// // This also gets called at build time
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
     
-	// const slug = context?.query?.slug ? context.query.slug : 'test-article-three'
     
-	const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/all-components.json`);
+    const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/all-components.json`);
     const data = await res.json();
-
-	// console.log(data);
-	// grab the preview from context
 
     const preview = context.preview;
     let prevData;
 
-	// if is preview, call home to get the latest draft, by pinging the element-api and passing along the same token we did with thw original
-	// request.
-
     if(preview){
+        console.log('preview is true');
         const previewData = context.previewData;
-        const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/all-components.json?token=${previewData['token']}`);
+        const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/all-components?token=${previewData['token']}`);
         prevData = await prevResponse.json()
-	}
-	
-	// set the page data based on preview. If preview mode, get the draft stuff, and if not, get the last published entry in the db
-
+        
+    } 
     let page = preview ? prevData : data;
 
-    if (!page) {
-		return {
-			notFound: true,
-		}
-    }
     return {
-		props: {
-			preview: preview ? true : false,
-			page: page
-		}
-	};
-};
-
+        props: { 
+            preview: preview ? true : false,
+            page: page
+        }
+    };
+}
 
 export default AllComponents; 
-
-
