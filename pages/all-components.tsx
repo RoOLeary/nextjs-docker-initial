86d7122fa4c8ab61fd9f66e0b29432a47c061aa7
Ronan <roleary81@gmail.com>
2022-11-19 22:29:33 +0100

    revert fetch change

--- pages/all-components.tsx
+++ pages/all-components.tsx
@@ -1,67 +1,85 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import MainLayout from './../components/Globals/Layouts/MainLayout'
-import PageBlocks from './../components/PageBlocks'
-import { GetServerSideProps } from 'next'
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import MainLayout from '../components/Globals/Layouts/MainLayout'
+import Faq from '../components/Faq/Faq'
+import Hero from '../components/Hero'
+import BoxedImage from '../components/BoxedImage'
+import Button from '../components/Button'
+import Cards from '../components/Cards/Cards'
+// import Contact from '../components/Contact/Contact'
+import CTA from '../components/CTA/CTA'
+import Chapters from '../components/Chapters/Chapters'
+import MapComponent from '../components/Map/MapComponent'
+import MiniBanner from '../components/MiniBanner'
+import Note from '../components/Note'
+import PageBlocks from '../components/PageBlocks'
+import USPs from '../components/USPs'
+import { GetStaticProps } from 'next';
+import { useTypingText } from '../hooks/useTypingText';
 
-const AllComponents = ({ page, preview }:any) => {
+const AllComponents = ({ page, preview }) => {
+	
+	let content = page.pageBlocks;
 
-   
-    const router = useRouter()
-    const slug = router.query
-    const {title, body} = page;
+	return (
+		<>
+		<Head>
+			<title>PicNext Prototype</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<MainLayout>	
+			<main className={'main'}>
+				{preview ? `previewing ${page.title}` : ''}
+				<h1>{page.title}</h1>
+				<PageBlocks content={content} />
+			</main>
+		</MainLayout>
+		</>
+	)
+}
 
-    let content = page.blocks;
-    console.log(page);
-    return (
-        <MainLayout>
-           {preview ? <h1>In Preview Mode</h1> : null}
-            <section className="b-text  c-section" id="learn-more">
-                <div className="o-wrapper">
-                    
-                    <div>
-                        <h1>{title}</h1>
-                        <div className={"c-formatted"} dangerouslySetInnerHTML={{__html: body }} />  
-                    </div>
-                </div>
-               
-            </section>
-            
-            <PageBlocks content={content} />
 
-            {/* <Related related={entry.manualRelatedArticles} currentslug={entry.slug} /> */}
-        
-        </MainLayout>
-    )
-  }
-  
-// // This also gets called at build time
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
+    // console.log(context);
     
-    const slug = context.query.slug ? context.query.slug : 'all-components'
-    let sectionHandle = 'pages';
-    const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/${sectionHandle}/${slug}.json`);
+	// const slug = context?.query?.slug ? context.query.slug : 'test-article-three'
+    
+	const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/all-components.json`);
     const data = await res.json();
+
+	// console.log(data);
+	// grab the preview from context
 
     const preview = context.preview;
     let prevData;
 
+	// if is preview, call home to get the latest draft, by pinging the element-api and passing along the same token we did with thw original
+	// request.
+
     if(preview){
-        console.log('preview is true');
         const previewData = context.previewData;
-        const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/${sectionHandle}/${slug}.json?token=${previewData['token']}`);
+        const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/all-components.json?token=${previewData['token']}`);
         prevData = await prevResponse.json()
-        
-    } 
+	}
+	
+	// set the page data based on preview. If preview mode, get the draft stuff, and if not, get the last published entry in the db
+
     let page = preview ? prevData : data;
 
+    if (!page) {
+		return {
+			notFound: true,
+		}
+    }
     return {
-        props: { 
-            preview: preview ? true : false,
-            page: page
-        }
-    };
-}
+		props: {
+			preview: preview ? true : false,
+			page: page
+		}
+	};
+};
+
 
 export default AllComponents; 
+
+
