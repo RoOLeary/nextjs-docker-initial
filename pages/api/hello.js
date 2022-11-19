1d8eb814082274d4f507f9aeab61ce00c4aade91
Ronan <roleary81@gmail.com>
2022-11-19 22:23:50 +0100

    try again

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,8 +5,6 @@ export default async function hello(req, res) {
   const randmchars = await fetch('https://rickandmortyapi.com/api/character/1,2');
   const results = await randmchars.json(); 
 
-
   res.status(200).json({ name: 'Ro', results: results })
-
 
 }
