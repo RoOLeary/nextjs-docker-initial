f48b98df22a7e6bf2e37b8d6e24c07d3e0bfa0f8
Ronan <roleary81@gmail.com>
2022-11-20 23:12:05 +0100

    add attempt to configure pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,48 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone',
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+module.exports = withPWA({
+  reactStrictMode: false,
+  trailingSlash: false,
+  distDir: '/build',
   i18n: {
-    // The locales you want to support in your app
-    locales: ["nl", "de", "fr"],
-    // The default locale you want to be used when visiting a non-locale prefixed path e.g. `/hello`
-    defaultLocale: "nl",
+    locales: ['en', 'fr', 'de', 'nl'],
+    defaultLocale: 'en',
   },
-}
+  images: {
+    loader: 'custom',
+    path: '/',
+    domains: ['placedog.net', 'source.unsplash.com', 'nextjs-docker-tau.vercel.app', 'localhost:3000'],
+  },
+  pwa: {
+    dest: 'public',
+    swSrc: 'service-worker.js',
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
+
+  exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/all-components': { page: '/all-components'}
+        }
+    },
+});
+
+
---
+++ pages/_document.tsx
@@ -0,0 +1,53 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+  } from "next/document";
+  import { ServerStyleSheet } from "styled-components";
+  
+  export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+      const sheet = new ServerStyleSheet();
+      const originalRenderPage = ctx.renderPage;
+  
+      try {
+        ctx.renderPage = () =>
+          originalRenderPage({
+            enhanceApp: (App) => (props) =>
+              sheet.collectStyles(<App {...props} />),
+          });
+  
+        const initialProps = await Document.getInitialProps(ctx);
+        return {
+          ...initialProps,
+          styles: (
+            <>
+              {initialProps.styles}
+              {sheet.getStyleElement()}
+            </>
+          ),
+        };
+      } finally {
+        sheet.seal();
+      }
+    }
+    render() {
+      return (
+        <Html lang="en">            
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+          <meta name="theme-color" content="#000" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>  
+          <Head />
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      );
+    }
+}
