629f342b3c268f2af0d2ea3ff4289c7630a0e3ad
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-21 00:18:25 +0100

    update download-app and sw for scratch demo. not looking totally woeful

--- pages/download-app.tsx
+++ pages/download-app.tsx
@@ -22,12 +22,6 @@ const DownLoadApp = () => {
                     layout="responsive"
                     
                     />
-                    <div className="o-wrapper">
-                        <div>
-                            <h1>Download the app</h1>
-                            <p>In the free Picnic app you can do all the shopping you need within a few minutes. From A-brand to private label, from fresh vegetables to shampoo. From 35 euros we will deliver everything for free!</p>
-                        </div>
-                    </div>
                 </div>
                 <section className="b-text c-section" id="about">
                     <div className="o-wrapper">
