aeeef170d6b75133f8d53d2207fce83aec6972d9
Ronan <roleary81@gmail.com>
2022-11-21 20:49:16 +0100

    update the about standalone instance to use SSG

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,42 +1,33 @@
-import Head from 'next/head'
 import MainLayout from '../components/Globals/Layouts/MainLayout'
 import PageBlocks from '../components/PageBlocks'
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 const About = ({ page, preview }:any) => {
 
   const { title } = page;
   const content = page.pageBlocks;
-	return(
-		<>
-			<Head>
-				<title>About us | PicNext Prototype</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<MainLayout>
-				{preview ? <h3>Preview Mode</h3> : null}
-				<PageBlocks content={content} />
-			</MainLayout>
-		</>
-	)
+  return(
+    <MainLayout>
+      {preview ? <h1>In Preview Mode</h1> : null}
+      <PageBlocks content={content} />
+    </MainLayout>
+  )
 }
 
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  // console.log(context);
+export const getStaticProps: GetStaticProps = async ({ preview = false, previewData }) => {
+  
+  // console.log(params)
   const res = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/about.json`);
   const data = await res.json();
 
-  const preview = context.preview;
   let prevData;
-
-  if(preview){
-    const previewData = context.previewData;
-    // console.log(previewData['token']);
+  if(preview && previewData){
     const prevResponse = await fetch(`https://servd-test-staging.cl-eu-west-3.servd.dev/api/pages/about.json?token=${previewData['token']}`);
     prevData = await prevResponse.json()
   } 
   let page = preview ? prevData : data;
+
 
   return {
 	props: {
@@ -45,6 +36,9 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 	}
   };
 };
+
+
+
 
 
 export default About; 
