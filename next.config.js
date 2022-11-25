85e2165f480eb7abf6e3e8819ee83c098525879d
Ronan O'Leary <ronanoleary@Ronan-OLearys-MacBook-Pro.local>
2022-11-25 09:36:19 +0100

    test isr-publish extension

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ module.exports = withPWA({
     locales: ['en', 'fr', 'de', 'nl'],
     defaultLocale: 'en',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     loader: 'custom',
     path: '/',
