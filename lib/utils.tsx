444b9abe4caf4cb193fe5a22810cce44dacd7cf5
Ronan <roleary81@gmail.com>
2022-11-29 22:34:02 +0100

    makes some updates to render the questions block on a page, rather than the component version

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -119,3 +119,93 @@ export const jobs: { id: number, jobCategory: string; jobImage: string, jobLink:
         jobImage: 'assets/images/people.webp'
     },
 ]
+
+
+
+export const faqs: { question: string, answer: string; }[] = [
+
+    {
+        "question": "How does the queue work?",
+        "answer": "If you create an account with us and you live in a postal code that falls within our delivery area, we will of course prefer to visit you the next day. \n Still, you first get in the queue. We do this so that we can expand in small steps and everyone always receives the very best service. Fortunately, once there, never again in line!"
+    },
+    {
+        "question": "How do I place an order and pay?",
+        "answer": "Once you have access to the app, you can start shopping. "
+    },
+    {
+        "question": "How do I know if the products are fresh?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Until what time can I place my order for tomorrow?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Can I still add and remove products after I have already ordered?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Can I still change my delivery time?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Is there a minimum order amount?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "How long are promotions valid?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "What are the delivery times?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Can someone under the age of 18 take an order?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "What if I am not at home when the delivery person rings the doorbell?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Is it intended to tip the delivery driver?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Can I return deposit bottles and bags?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Can I return my glass to Picnic?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "What else do you recycle?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Do you take back return packages from other webshops?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "What if there is something wrong with a product I ordered?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Why do I have to log in with an SMS code?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "What about my right of withdrawal?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "Is my data safe with you?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    },
+    {
+        "question": "How and when can I reach you?",
+        "answer": "This is just some dummy text, as the actual answers are rather long"
+    }
+]
--- pages/faqs/index.tsx
+++ pages/faqs/index.tsx
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import Head from 'next/head';
 import MainLayout from '../../components/Globals/Layouts/MainLayout';
 import StaticHeader from '../../components/StaticHeader';
+import FaqItem from '../../components/Faq/FaqItem';
+import { faqs } from './../../lib/utils'; 
 
-export default function FAQs() {
+const FAQs = () => {
+
+    const selectorAnswerInner: any = '.js-faqAnswerInner';
+    
+    const onQuestionClick = (e: any) => {
+        const question = e.target;
+        const classNameIsExpanded = 'is-expanded';
+        question.parentElement.classList.toggle(classNameIsExpanded);
+    };
+
+    useEffect(() => {
+        document.querySelectorAll(selectorAnswerInner).forEach(answerInner => {
+            answerInner.parentElement.style.setProperty('--height', answerInner.offsetHeight + 'px')
+        })
+    },[]);
+
     return(
         <>
             <Head>
@@ -11,11 +29,23 @@ export default function FAQs() {
             </Head>
             <MainLayout>
                 <StaticHeader content={'FAQs List'} />
-                <div className={'faqs'}>
-                    <p>FAQs List</p>
-                </div>
+                <section className="c-section">
+                    <div className="o-wrapper o-wrapper--narrow">
+                        <div className={'faqs'}>
+                            <ol className="b-faq__list">
+                                {faqs.map((faq, i) => {
+                                    return(
+                                        <FaqItem key={i} faq={faq} onClick={(e: any) => onQuestionClick(e)} />
+                                    )
+                                })}
+                            </ol>
+                        </div>
+                    </div>
+                </section>
             </MainLayout>
         </>
     )
 
 }
+
+export default FAQs
